031e03663fde8899908d4048bcf4518ecf99f6a9
JaneJYLim <juyeonsh412@gmail.com>
2020-08-10 01:51:35 +0900

    0810_세계의미술작품

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function fnLayer() {
+    const layer = document.getElementById("layerBackground");
+        layer.style.display="block";    
+}
+
+function fnClose() {
+    const close = document.getElementById("layerBackground");
+        close.style.display="none";
+}
