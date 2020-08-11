9c0c1fad5f2c132be584ba89a2027144f0e9b3ee
JaneL <67691664+JaneJYLim@users.noreply.github.com>
2020-08-11 18:15:15 +0900

    0811_세계의미술작품 v1.1
    subMenu jQuery구현

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
-function fnLayer() {
-    const layer = document.getElementById("layerBackground");
-        layer.style.display="block";    
+//풀다운 네비게이션 메뉴 시작
+$("#mainMenu>li").mouseover(function(){
+    $(this).children("ul").stop().fadeIn(1000);
+});
+
+$("#mainMenu>li").mouseout(function(){
+    $(this).children("ul").stop().fadeOut(1000);
+});
+
+var layerBg = document.getElementById("layerPopupBg"); 
+
+function fnLayerPopup() {
+    layerBg.style.display = "block";
 }
 
+
 function fnClose() {
-    const close = document.getElementById("layerBackground");
-        close.style.display="none";
+    layerBg.style.display = "none";
 }
+
+document.getElementById("closeBtn").onclick = fnClose;
